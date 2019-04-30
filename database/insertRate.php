7ba0ace9f4a5f9353f0376dbe72ac26583ffeb94
Maph Niet <lucthuyvip@gmail.com>
2019-04-30 19:18:34 +0700

    add rate and comment

---
+++ database/insertRate.php
@@ -0,0 +1,23 @@
+
+<?php
+        session_start();
+        include("../database/connectDB.php");
+        if(isset($_POST['input'])){
+        $dat = '30-04-2019';
+        $content =$_POST['input'];
+        $accID = 2;
+        $proID = $_SESSION['product']['proID'];
+        $rate = $_POST['rating'];
+        $conn = connectDB();
+        $query = 'insert into rate_product(accID, proID, point) values ('.$accID.",".$proID.",".$rate.")";
+        $query2 = "insert into comment_product(accID, proID, content,created) values (".$accID.",".$proID.",'".$content."','".$dat."')";
+        echo $query;
+        echo $query2;
+        mysqli_query($conn,$query);
+        mysqli_query($conn,$query2);
+       
+        }
+	
+
+	
+?>
