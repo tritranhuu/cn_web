804fc0c9220ade56e3c8dd6ba9cbdb629a696d83
tritranhuu <tritranhuu123@gmail.com>
2019-05-01 14:26:28 +0700

    fix again

--- php/admin/index.php
+++ php/admin/index.php
@@ -1,5 +1,9 @@
 <?php
-
+session_start();
+if (!isset($_SESSION['admin']))
+{
+    header('Location: ../index.php');
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
--- php/category.php
+++ php/category.php
@@ -55,42 +55,7 @@ include("../database/dbCart.php");
 	<div class="container">
 	<div class="col">
 	';
-?>
-<div class="row products_bar_row">
-					<div class="col">
-						<div class="products_bar d-flex flex-lg-row flex-column align-items-lg-center align-items-start justify-content-lg-start justify-content-center">
-							<div class="products_bar_links">
-							<div class="products_dropdown text-right product_dropdown_filter">
-									
-									
-								</div>
-							</div>
-							<div class="products_bar_side d-flex flex-row align-items-center justify-content-start ml-lg-auto">
-								<div class="products_dropdown product_dropdown_sorting">
-									<div class="isotope_sorting_text"><span>Default Sorting</span><i class="fa fa-caret-down" aria-hidden="true"></i></div>
-									<ul>
-										<li class="item_sorting_btn" data-isotope-option='{ "sortBy": "original-order" }'>Default</li>
-										<li class="item_sorting_btn" data-isotope-option='{ "sortBy": "price" }'>Price</li>
-										<li class="item_sorting_btn" data-isotope-option='{ "sortBy": "name" }'>Name</li>
-									</ul>
-								</div>
-								
-								<div class="products_dropdown text-right product_dropdown_filter">
-									<div class="isotope_filter_text"><span>Filter</span><i class="fa fa-caret-down" aria-hidden="true"></i></div>
-									<ul>
-										<li class="item_filter_btn" data-filter="*">All</li>
-										<li class="item_filter_btn" data-filter=".lon"> >=200000</li>
-										<li class="item_filter_btn" data-filter=".nho"> <200000 </li>
-										
-									</ul>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-           </div>
-           </div>
-<?php
+	require('filter.php');
 	echo '<div class="row products_row products_container grid">';
     for ($i = 12*($_SESSION['page']-1),$t=0 ;$i < $_SESSION['max'] ; $i++,$t++){
 		if($t==12 ) break;
